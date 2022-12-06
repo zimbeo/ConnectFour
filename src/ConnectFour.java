cd38f164e8139e8de689587e6e6bc6b940419875
Henry Rohlfing <admin@rohlfing.systems>
2022-12-06 10:00:38 -0600

    Commit final solution

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -1,5 +1,5 @@
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 public class ConnectFour {
 
@@ -21,7 +21,7 @@ public class ConnectFour {
     public static void showGameBoard(char[][] gameBoard) {
 
         // Show the initialized 2d array by looping through and showing each value
-        System.out.println("   0 1 2 3 4 5 6\n  ---------------");
+        System.out.println("  0 1 2 3 4 5 6\n  ---------------");
 
         for (int i = 0; i < gameBoard.length; i++) {
 
@@ -33,10 +33,15 @@ public class ConnectFour {
 
             System.out.println("\n  ---------------");
         }
-        System.out.println("   0 1 2 3 4 5 6 ");
+        System.out.println("  0 1 2 3 4 5 6 ");
     }
 
     public static boolean checkTurnValidity(int column, char[][] gameBoard) {
+
+        // Check if the spot the user is putting the token into is full
+        if ((column < gameBoard.length) && (column > 0) && (gameBoard[0][column] != ' ')) {
+            return false;
+        }
 
         // Check if column fits dimensions of the game board
         if (column > gameBoard.length || column < 0) {
@@ -55,8 +60,8 @@ public class ConnectFour {
         if (checkTurnValidity(playedColumn, gameBoard)) {
 
             // If the turn was valid, loop through board, and decrement row as necessary
-            for (int row = gameBoard.length - 1; row >= 0; row--){
-                if(gameBoard[row][playedColumn] == ' '){
+            for (int row = gameBoard.length - 1; row >= 0; row--) {
+                if (gameBoard[row][playedColumn] == ' ') {
                     gameBoard[row][playedColumn] = userTokenColor;
                     break;
                 }
@@ -67,7 +72,7 @@ public class ConnectFour {
             // Return true so main method knows turn was successfully played
             return true;
         } else {
-            System.out.println("ERROR: Your turn was invalid, try again.");
+            System.out.println("ERROR: Your turn was invalid or the column was full, try again.");
             return false;
         }
     }
@@ -89,14 +94,14 @@ public class ConnectFour {
         Random rand = new Random();
 
         // Generate a random column that the computer will play, set bound to 8 as there is 7 columns
-        int columnUpperBound = 8;
+        int columnUpperBound = 7;
         int randomColumn = rand.nextInt(columnUpperBound);
 
         if (checkTurnValidity(randomColumn, gameBoard)) {
 
             // If the turn was valid, loop through board, and decrement row as necessary
-            for (int row = gameBoard.length - 1; row >= 0; row--){
-                if(gameBoard[row][randomColumn] == ' '){
+            for (int row = gameBoard.length - 1; row >= 0; row--) {
+                if (gameBoard[row][randomColumn] == ' ') {
                     gameBoard[row][randomColumn] = compTokenColor;
                     System.out.println("\nComputer played token in row " + row + " and column " + randomColumn + "\n");
                     break;
@@ -105,6 +110,63 @@ public class ConnectFour {
 
             showGameBoard(gameBoard);
         }
+    }
+
+    public static boolean checkWin(char[][] gameBoard, char token) {
+
+        // Get 4 in a vertical row (up or down)
+        for (int row = 0; row < gameBoard.length - 3; row++) {
+            for (int column = 0; column < gameBoard[0].length; column++) {
+                if (
+                        gameBoard[row][column] == token &&
+                                gameBoard[row + 1][column] == token &&
+                                gameBoard[row + 2][column] == token &&
+                                gameBoard[row + 3][column] == token) {
+                    return true;
+                }
+            }
+        }
+
+        // Check diagonal (up)
+        for (int row = 3; row < gameBoard.length; row++) {
+            for (int column = 0; column < gameBoard[0].length - 3; column++) {
+                if (
+                        gameBoard[row][column] == token &&
+                                gameBoard[row - 1][column + 1] == token &&
+                                gameBoard[row - 2][column + 2] == token &&
+                                gameBoard[row - 3][column + 3] == token) {
+                    return true;
+                }
+            }
+        }
+
+        // Check diagonal (down)
+        for (int row = 0; row < gameBoard.length - 3; row++) {
+            for (int column = 0; column < gameBoard[0].length - 3; column++) {
+                if (
+                        gameBoard[row][column] == token &&
+                                gameBoard[row + 1][column + 1] == token &&
+                                gameBoard[row + 2][column + 2] == token &&
+                                gameBoard[row + 3][column + 3] == token) {
+                    return true;
+                }
+            }
+        }
+
+        // Get 4 in horizontal row (left or right)
+        for (int row = 0; row < gameBoard.length; row++) {
+            for (int column = 0; column < gameBoard[0].length - 3; column++) {
+                if (
+                        gameBoard[row][column] == token &&
+                                gameBoard[row][column + 1] == token &&
+                                gameBoard[row][column + 2] == token &&
+                                gameBoard[row][column + 3] == token) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     public static void main(String[] args) {
@@ -119,20 +181,26 @@ public class ConnectFour {
         showGameBoard(gameBoard);
 
         while (true) {
-            if (turn == userTokenColor) {
+            if ((turn == userTokenColor) & (!checkWin(gameBoard, userTokenColor))) {
                 playedTurn = playUserTurn(scanner, gameBoard, turn);
 
                 // Validates if the play was actually successful
                 if (playedTurn) {
                     turn = setTurn(turn);
                 }
+            } else if (checkWin(gameBoard, userTokenColor)) {
+                System.out.println("Token: " + userTokenColor + " (User) won the game! Get better computer!");
+                break;
             }
 
-            if (turn == compTokenColor) {
+            if ((turn == compTokenColor) & (!checkWin(gameBoard, compTokenColor))) {
                 playComputerTurn(scanner, gameBoard, compTokenColor);
 
                 // Assume the computer played a valid turn since it plays a random number with an upper bound
                 turn = setTurn(turn);
+            } else if (checkWin(gameBoard, compTokenColor)) {
+                System.out.println("Token: " + compTokenColor + " (Computer) won the game! Get better user!");
+                break;
             }
         }
     }
